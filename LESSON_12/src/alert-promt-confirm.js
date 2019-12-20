97fcd5a9d6c38f24161f5de59d3543f33ce73d98
LionStep1 <lionstepancow@gmail.com>
2019-12-20 20:31:32 +0300

    add: Alert Promt Confirm

---
+++ LESSON_12/src/alert-promt-confirm.js
@@ -0,0 +1,10 @@
+'use strict';
+ //let x = alert('Alert Promt Confirm');
+//let x = prompt('Сколько тебе лет?');
+//let x = confirm('Ты самый умный?');
+//console.log(`Значение X: ${x}`);
+
+let name = prompt('Kak tvoe im9?');
+
+
+console.log(`Privet , ${name}`);
