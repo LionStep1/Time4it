ba4aaa555dd52edd2bcb19b6d4182d18b3d5a052
LionStep1 <lionstepancow@gmail.com>
2019-12-20 21:34:58 +0300

    add If Operator

---
+++ LESSON_12/src/if-operator.js
@@ -0,0 +1,28 @@
+//let year = prompt('Какой год?');
+//if (year == 2019) {console.log('А ты чертовски прав!')};
+
+//else {
+ // console.log( 'А вот нет\'');
+
+
+//if (year == 2019) {
+  //console.log('Ты прав!');
+  
+ //else if (year > 2019) {
+  //console.log('Ты что из будущего?');
+
+ //else if (year === null) {
+   //console.log('Ты что шпион');
+ //}
+  
+//} else if (year<2019{
+//  console.log('Ты что из прошлого?');
+//}
+
+let name = prompt('Какое первое название Джава Скрипта?');
+if (name == 'ACMA SCRIPT') {
+  console.log('Верно!!');
+  
+}else {
+  console.log('Не Верно!');
+}
