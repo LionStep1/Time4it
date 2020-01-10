481b4b08316a35db5a858003f8d815dcf3a69986
LionStep1 <lionstepancow@gmail.com>
2020-01-10 22:06:23 +0300

    edit: calculator.js learn objects

---
+++ LESSON_13/calculator/calculator.js
@@ -0,0 +1,23 @@
+'use strict';
+//let x = +prompt('Задать  число X:', '0');
+//let y = +prompt('Задать  число Y:'), '0');
+//
+//function sum(x,y){
+//  return x + y;
+//}
+//function mult(x,y){
+//  return x * y;
+//}
+
+
+let user = {
+  "49": "Германия",
+  "41": "Швейцария",
+  "44": "Великобритания",
+  "1": "США"
+}
+let user = {};
+user.name = "Jonh";
+user.surname = "Smith";
+user.name = "Pete";
+delete name
